2805b10e1567777c150d0b950bad7162a667fa31
Pierre-Emmanuel41 <pierre.emmanuel41@gmail.com>
2021-09-06 00:52:07 +0200

    mumble-common: Adding class ServerJoinInterpreter

--- src/main/java/fr/pederobien/mumble/server/impl/responses/ServerJoinResponse.java
+++ src/main/java/fr/pederobien/mumble/server/impl/responses/ServerJoinResponse.java
@@ -1,11 +1,20 @@
 package fr.pederobien.mumble.server.impl.responses;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import fr.pederobien.messenger.interfaces.IMessage;
 import fr.pederobien.mumble.common.impl.ErrorCode;
 import fr.pederobien.mumble.common.impl.Header;
 import fr.pederobien.mumble.common.impl.MumbleMessageFactory;
+import fr.pederobien.mumble.common.impl.Oid;
 import fr.pederobien.mumble.server.event.RequestEvent;
 import fr.pederobien.mumble.server.impl.InternalServer;
+import fr.pederobien.mumble.server.impl.SoundManager;
+import fr.pederobien.mumble.server.interfaces.IChannel;
+import fr.pederobien.mumble.server.interfaces.IPlayer;
+import fr.pederobien.mumble.server.interfaces.ISoundModifier;
 
 public class ServerJoinResponse extends AbstractResponse {
 
@@ -18,7 +27,60 @@ public class ServerJoinResponse extends AbstractResponse {
 		switch (event.getRequest().getHeader().getOid()) {
 		case SET:
 			event.getClient().onJoin();
-			return event.getRequest().answer(event.getRequest().getPayload());
+			List<Object> informations = new ArrayList<Object>();
+
+			// UDP port number
+			informations.add(getInternalServer().getUdpPort());
+
+			// Number of channels
+			informations.add(getInternalServer().getChannels().size());
+			for (IChannel channel : getInternalServer().getChannels().values()) {
+				// Channel name
+				informations.add(channel.getName());
+
+				// Channel's sound modifier name
+				informations.add(channel.getSoundModifier().getName());
+
+				// Number of players
+				informations.add(channel.getPlayers().size());
+
+				for (IPlayer player : channel.getPlayers()) {
+					// Player name
+					informations.add(player.getName());
+
+					// Player's mute status
+					informations.add(player.isMute());
+
+					// Player's deafen status
+					informations.add(player.isDeafen());
+				}
+			}
+
+			// Number of sound modifier
+			Map<String, ISoundModifier> modifiers = SoundManager.getSoundModifiers();
+			informations.add(modifiers.size());
+
+			// Modifier informations
+			for (ISoundModifier modifier : modifiers.values())
+				// Modifier's name
+				informations.add(modifier.getName());
+
+			// Player identifier
+			informations.add(event.getClient().getUUID());
+
+			boolean playerConnected = event.getClient().getPlayer() != null;
+			if (playerConnected) {
+				// Player online status
+				informations.add(true);
+
+				// Player name
+				informations.add(event.getClient().getPlayer().getName());
+
+				// Player admin status
+				informations.add(event.getClient().getPlayer().isOnline());
+			} else
+				informations.add(false);
+			return MumbleMessageFactory.answer(event.getRequest(), Oid.INFO, informations.toArray());
 		default:
 			return MumbleMessageFactory.answer(event.getRequest(), ErrorCode.INCOMPATIBLE_IDC_OID);
 		}
