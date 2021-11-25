e3971ce3e8452ee2fa3bc50681867cad92583a9a
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2021-11-25 12:37:33 +0100

    Adding radius parameter for LinearCircularSoundModifier

--- src/main/java/fr/pederobien/mumble/server/impl/modifiers/LinearCircularSoundModifier.java
+++ src/main/java/fr/pederobien/mumble/server/impl/modifiers/LinearCircularSoundModifier.java
@@ -1,23 +1,50 @@
 package fr.pederobien.mumble.server.impl.modifiers;
 
 import fr.pederobien.mumble.server.impl.MathHelper;
+import fr.pederobien.mumble.server.interfaces.IParameter;
 import fr.pederobien.mumble.server.interfaces.IPlayer;
 
 public class LinearCircularSoundModifier extends AbstractSoundModifier {
-	private double slope;
+	public static final String RADIUS_PARAMETER_NAME = "Radius";
+	private IParameter<Double> radiusParameter;
 
-	public LinearCircularSoundModifier(double radius) {
-		super(String.format("LinearCircular_%s", radius < 0 ? -radius : radius));
-		slope = -1.0 / (radius < 0 ? -radius : radius);
+	/**
+	 * Creates a linear circular sound modifier. The first adjective <code>linear</code> refers how the global sample volume will
+	 * evolve according to the distance between two players. The second adjective <code>circular</code> refers how the stereo will
+	 * evolve according to the yaw between the two players. For this sound modifier, the stereo depends only on the yaw between the
+	 * receiving player and the transmitting player the yaw between the transmitting player and the receiving player has no influence
+	 * on the stereo. The default radius is 50 which means that from a distance of 0 to a distance of 50 the global sample volume will
+	 * decrease linearly from 1 (maximum volume) to 0. A {@link Parameter} for the radius is created associated to the name
+	 * {@value #RADIUS_PARAMETER_NAME}.
+	 */
+	public LinearCircularSoundModifier() {
+		super("LinearCircular");
+		getParameters().add(radiusParameter = RangeParameter.of(this, RADIUS_PARAMETER_NAME, 50.0, 1.0, Double.MAX_VALUE));
 	}
 
 	@Override
 	public VolumeResult calculate(IPlayer transmitter, IPlayer receiver) {
 		if (transmitter.equals(receiver))
 			return VolumeResult.DEFAULT;
-		
+
 		double distance = MathHelper.getDistance3D(transmitter.getPosition(), receiver.getPosition());
 		double[] volumes = MathHelper.getDefaultLeftAndRightVolume(transmitter.getPosition(), receiver.getPosition());
-		return new VolumeResult(slope * distance + 1, volumes[0], volumes[1]);
+		return new VolumeResult((-1.0 / radiusParameter.getValue()) * distance + 1, volumes[0], volumes[1]);
+	}
+
+	/**
+	 * @return The radius beyond which two players can no longer talk together.
+	 */
+	public double getRadius() {
+		return radiusParameter.getValue();
+	}
+
+	/**
+	 * Set the radius beyond which two players can no longer talk together.
+	 * 
+	 * @param radius The new radius.
+	 */
+	public void setRadius(double radius) {
+		radiusParameter.setValue(radius);
 	}
 }
